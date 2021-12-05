ee4aee12470debff2092422e7ed7eab1fd0ecd91
Mikołaj Chełpa <mikolaj.chelpa@gmail.com>
2021-12-05 08:45:35 +0100

    created own repl and basic database persistence

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,74 @@ pub mod schema;
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
+extern crate lazy_static;
 
 use self::models::{Message, NewMessage};
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
-use diesel::r2d2;
+use diesel::r2d2::{ConnectionManager, Pool, PoolError, PooledConnection};
 use dotenv::dotenv;
+use lazy_static::lazy_static;
 use std::env;
+use teloxide::dispatching::Dispatcher;
 use teloxide::prelude::*;
+use tokio_stream::wrappers::UnboundedReceiverStream;
+
+type PgPool = Pool<ConnectionManager<PgConnection>>;
+type PgPooledConnection = PooledConnection<ConnectionManager<PgConnection>>;
+
+struct Db {
+    pool: PgPool,
+}
+
+impl Db {
+    fn get_connection(&self) -> Result<PgPooledConnection, PoolError> {
+        self.pool.get()
+    }
+}
+
+// TODO: pool size parameter as environment var
+lazy_static! {
+    static ref DB: Db = {
+        let cm = ConnectionManager::<PgConnection>::new(get_connection_string());
+        Db {
+            pool: Pool::builder().max_size(4).build(cm).unwrap(),
+        }
+    };
+}
 
 #[tokio::main]
 async fn main() {
+    run().await;
+}
+
+async fn run() {
     teloxide::enable_logging!();
 
     log::info!("Starting Lokaj Bot");
     let bot = Bot::from_env().auto_send();
 
-    let pg_connection_manager = r2d2::ConnectionManager::new(get_connection_string());
-    let pg_connection_pool = r2d2::Pool::builder()
-        .max_size(4)
-        .build(pg_connection_manager)
-        .unwrap();
+    let dp = Dispatcher::new(bot).messages_handler(
+        |rx: DispatcherHandlerRx<AutoSend<Bot>, teloxide::prelude::Message>| async move {
+            UnboundedReceiverStream::new(rx)
+                .for_each_concurrent(None, |message| async move {
+                    log::info!("Received a message!");
+                    let user_id = message.update.from().unwrap().id;
+                    let text = message.update.text().unwrap();
 
-    teloxide::repl(bot, |message| async move {
-        log::debug!("{:#?}", message.update.from());
-        log::debug!("{:#?}", message.update.text());
+                    log::info!("Saving message to database...");
+                    receive_message(&user_id, &text).await;
 
-        let user_id = message.update.from().unwrap().id;
-        let text = message.update.text().unwrap();
-        log::info!("Connecting to PostgreSQL");
-        let connection = pool.get().unwrap();
-        receive_message(&connection, &user_id, &text);
+                    message
+                        .answer_dice()
+                        .await
+                        .expect("Error while replying with dice");
+                })
+                .await;
+        },
+    );
 
-        message.answer_dice().await?;
-        respond(())
-    })
-    .await;
+    dp.dispatch().await;
 }
 
 fn get_connection_string() -> String {
@@ -61,7 +93,7 @@ fn get_connection_string() -> String {
     }
 }
 
-fn receive_message<'a>(conn: &PgConnection, user_id: &'a i64, text: &'a str) -> Message {
+async fn receive_message<'a>(user_id: &'a i64, text: &'a str) {
     use schema::messages;
 
     let new_message = NewMessage {
@@ -69,8 +101,14 @@ fn receive_message<'a>(conn: &PgConnection, user_id: &'a i64, text: &'a str) -> 
         text: text,
     };
 
+    let conn = DB
+        .get_connection()
+        .expect("Error retrieving connection from the pool");
+
     diesel::insert_into(messages::table)
         .values(&new_message)
-        .get_result(conn)
-        .expect("Error saving received message")
+        .get_result::<Message>(&conn)
+        .expect("Error saving received message");
+
+    log::info!("Message saved!");
 }
